bbc78f90fd9b8299d33265fcd4d59ae255ba0fe9
Adrian <adrian.szablowski.kontakt@gmail.com>
2023-05-15 23:23:59 +0200

    changing border color every 4 seconds

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello from Nerdbord!')
+// console.log('Hello from Nerdbord!')
+
+const box = document.querySelector('.box')
+
+let i = 0
+
+const changingColorBox = () => {
+	const colors = ['red', 'blue', 'yellow', 'green']
+	box.style.borderColor = colors[i]
+	i = (i + 1) % colors.length
+}
+
+setInterval(changingColorBox, 4000)
